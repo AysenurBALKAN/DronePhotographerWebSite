7ba6f9bf9118fe196368c4e66e1ba92e33458bef
AysenurBALKAN <balkan.aysenur@gmail.com>
2023-12-31 18:11:26 +0300

    admin-panel-prepared

---
+++ Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DronePhotographerWebSite.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int ToDoId { get; set; }
+        public string ToDoDescription { get; set; }
+    }
+}
